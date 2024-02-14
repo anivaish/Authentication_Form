a129385f3b0085607e07fad5365643dc4d653dc7
Aniruddh Vaish <aniruddhvaish2024@gmail.com>
2024-02-15 01:01:34 +0530

    intial commit

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,10 +1,45 @@
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Main = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
 	};
+
+	const [image, setImage] = useState(null);
+	const [allImage, setAllImage] = useState(null);
+
+	useEffect(() => {
+		getImage();
+	}, []);
+	const submitImage = async (e) => {
+		e.preventDefault();
+
+		const formData = new FormData();
+		formData.append("image", image);
+
+		const result = await axios.post(
+			"http://localhost:8080/upload-image",
+			formData,
+			{
+				headers: { "Content-Type": "multipart/form-data" },
+			}
+		);
+	};
+
+	const onInputChange = (e) => {
+		console.log(e.target.files[0]);
+		setImage(e.target.files[0]);
+	};
+
+	const getImage = async () => {
+		const result = await axios.get("http://localhost:8080/get-image");
+		console.log(result);
+		setAllImage(result.data.data);
+	};
+
 
 	return (
 		<div className={styles.main_container}>
@@ -14,6 +49,21 @@ const Main = () => {
 					Logout
 				</button>
 			</nav>
+			<form onSubmit={submitImage}>
+				<input type="file" accept="image/*" onChange={onInputChange}></input>
+				<button type="submit">Submit</button>
+			</form>
+			{allImage == null
+				? ""
+				: allImage.map((data) => {
+					return (
+						<img
+							src={require(`./images/${data.image}`)}
+							height={200}
+							width={200}
+						/>
+					);
+				})}
 		</div>
 	);
 };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+
+//importing schema
+require("./models/imageDetails");
+const Images = mongoose.model("ImageDetails");
 
 // database connection
 connection();
@@ -19,3 +24,43 @@ app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
+
+
+// //////////////////////////////////////////////////
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "C:/Users/Aniruddh Vaish/Desktop/Keshav_proj/AuthInMern/client/src/components/Main/images/");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now();
+        cb(null, uniqueSuffix + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+
+app.post("/upload-image", upload.single("image"), async (req, res) => {
+    console.log(req.body);
+    const imageName = req.file.filename;
+
+    try {
+        await Images.create({ image: imageName });
+        res.json({ status: "ok" });
+    } catch (error) {
+        res.json({ status: error });
+    }
+});
+
+app.get("/get-image", async (req, res) => {
+    try {
+        Images.find({}).then((data) => {
+            res.send({ status: "ok", data: data });
+        });
+    } catch (error) {
+        res.json({ status: error });
+    }
+});
